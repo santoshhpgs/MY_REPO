b18ba63173bdce1982a6a87ad5f85dcb08bb00fa
Santosh Kumarachary <santosh.kumarachary@synapse.com>
2020-11-20 08:10:05 +0530

    new changes

--- java-programs/src/com/personal/strings/StringCheck.java
+++ java-programs/src/com/personal/strings/StringCheck.java
@@ -1,12 +1,21 @@
 package com.personal.strings;
 
+import java.util.Arrays;
+
 public class StringCheck {
 
 	public static void main(String[] args) {
-		String s1="checking";
+		/*String s1="checking";
 		String s2=new String("checking");
 		System.out.println(s1.equals(s2));
-		System.out.println(s1==s2);
+		System.out.println(s1==s2);*/
+		 /*String[] severity = new String[] {"critical","major","minor","warning"};
+		 System.out.println(Arrays.asList(severity).contains("critical"));*/
+		
+		long l = 123456;
+		String val = "";
+		System.out.println(val.isEmpty());
+		
 	}
 
 }
