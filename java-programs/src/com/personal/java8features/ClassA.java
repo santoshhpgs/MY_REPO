9ce95eac9558520d7c1d33af0590d494f96f8d3b
santosh <santoshhpgs@gmail.com>
2022-06-04 23:23:50 +0530

    practice

---
+++ java-programs/src/com/personal/java8features/ClassA.java
@@ -0,0 +1,10 @@
+package com.personal.java8features;
+
+public class ClassA implements InterfA{
+
+	@Override
+	public void methodA() {		
+		System.out.println("method one execution....");
+	}
+
+}
---
+++ java-programs/src/com/personal/java8features/ClassB.java
@@ -0,0 +1,11 @@
+package com.personal.java8features;
+
+public class ClassB implements InterfB {
+
+	@Override
+	public void sum(int a, int b) {
+		System.out.println("sum is > "+(a+b));
+
+	}
+
+}
